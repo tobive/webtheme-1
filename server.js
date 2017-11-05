c678d7be69de4777428ff55ea482d9e6a0f9ce2f
tobive <knightsofkangouw@gmail.com>
2017-11-05 11:53:09 +0700

    edit color, moving animation

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 var PORT = process.env.PORT || 8000;
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'app')));
 
 app.listen(PORT, function() {
   console.log("Express listening on port ", PORT);
